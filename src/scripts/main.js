06dce88a46f6d9f58b07a6fa72c04de5788cef64
Vasyl <priymak17ukrvaha@gmail.com>
2025-02-25 15:34:29 +0200

    add solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,17 @@
 'use strict';
 
-document.addEventListener('click', (e) => {
-  // write code here
+const spider = document.querySelector('.spider');
+
+spider.style.position = 'absolute';
+
+const box = document.querySelector('.wall');
+
+box.addEventListener('click', (e) => {
+  const rect = box.getBoundingClientRect();
+
+  const x = e.clientX - rect.left;
+  const y = e.clientY - rect.top;
+
+  spider.style.top = `${y}px`;
+  spider.style.left = `${x}px`;
 });

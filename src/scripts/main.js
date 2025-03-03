1af28348d17397049ac70fee21035d6c182f8a4f
Vasyl <priymak17ukrvaha@gmail.com>
2025-03-03 20:53:27 +0200

    fix

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -5,8 +5,8 @@ const box = document.querySelector('.wall');
 const rect = box.getBoundingClientRect();
 
 box.addEventListener('click', (e) => {
-  const x = e.clientX - rect.left;
-  const y = e.clientY - rect.top;
+  const x = e.clientX - rect.left - 34;
+  const y = e.clientY - rect.top - 34;
 
   spider.style.top = `${y}px`;
   spider.style.left = `${x}px`;
